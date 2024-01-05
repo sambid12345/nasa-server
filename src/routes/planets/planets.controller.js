63b6cf2c5b419fb4eeb2461a6fe5189a0445a3a3
sambid <sambid.champati@infosys.com>
2024-01-05 20:10:46 +0530

    main: using pm2 for clustering Node process

--- src/routes/planets/planets.controller.js
+++ src/routes/planets/planets.controller.js
@@ -1,8 +1,14 @@
+const cluster = require("cluster");
+
 const planetsModel = require("../../models/planets.model");
 
 const getAllPlanets = async function (req, res, next) {
   const responseData = await planetsModel.readPlanetsData();
-  //   console.log(responseData);
+
+  // bocking code
+  // let expiredTimer = new Date().getTime() + 5000;
+  // while (new Date().getTime() < expiredTimer) {}
+
   return res.status(200).send(responseData);
 };
 
